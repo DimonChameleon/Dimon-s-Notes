be0eb6c79b8f9be6f3d6fd126b28ba0d37b35449
DimonChameleon <ddryomin@gmail.com>
2024-10-08 15:22:58 +0200

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const addNote = document.querySelector('.add')
-
 const notesModal = document.querySelector('.modal')
 
-addNote.onpointerup = () => notesModal.style.display = 'flex'
+addNote.onpointerup = () => {
+    notesModal.querySelector('input').value = ''
+    notesModal.querySelector('textarea').value = ''
+    notesModal.style.display = 'flex'
+}
 
 const closeNoteModal = () => notesModal.style.display = ''
+
+const saveNote = () => {
+    const name = notesModal.querySelector('input').value.trim()
+    const text = notesModal.querySelector('textarea').value.trim()
+
+    const note = { name, text }
+    localStorage.setItem('saved-note', JSON.stringify(note))
+
+    closeNoteModal()
+}
