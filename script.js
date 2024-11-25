32dc067f8f8f0b27d4a850229dadf075ff354716
DimonChameleon <ddryomin@gmail.com>
2024-11-25 17:32:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,137 +1,110 @@
-const addNote = document.querySelector('.add')
-const notesModal = document.querySelector('.modal')
+const addNote = document.querySelector(".add");
+const notesModal = document.querySelector(".modal");
 
 addNote.onpointerup = () => {
-    notesModal.querySelector('input').value = ''
-    notesModal.querySelector('textarea').value = ''
-    notesModal.style.display = 'flex'
-}
+  notesModal.querySelector("input").value = "";
+  notesModal.querySelector("textarea").value = "";
+  notesModal.style.display = "flex";
+};
 
-const closeNoteModal = () => notesModal.style.display = ''
+const closeNoteModal = () => (notesModal.style.display = "");
 
-const createNoteMarkup = note => {
-    const titleSpan = document.createElement('span')
-    titleSpan.textContent = note.title
+const createNoteMarkup = (note) => {
+  const titleSpan = document.createElement("span");
+  titleSpan.textContent = note.title;
+  titleSpan.className = "noteTitle"
 
-    const textSpan = document.createElement('span')
-    textSpan.textContent = note.text
+  const textSpan = document.createElement("span");
+  textSpan.textContent = note.text;
 
-    const li = document.createElement('li')
-    li.append(titleSpan, textSpan)
-    li.className = "note-static";
-    return li
-}
+  const panelDiv = document.createElement("div");
+  panelDiv.append(titleSpan, textSpan);
 
-const saveNote = () => {
-    const title = notesModal.querySelector('input').value.trim()
-    const text = notesModal.querySelector('textarea').value.trim()
+  const deleteIcon = document.createElement("span");
+  deleteIcon.classList = "material-symbols-outlined";
+  deleteIcon.textContent = "delete";
 
-    if (!title && !text) {
+  deleteIcon.onpointerup = () => {
+    const spans = li.querySelectorAll('div span')
+    const title = spans.item(0).textContent
+    const text = spans.item(1).textContent
+
+    const notes = JSON.parse(localStorage.getItem("saved-notes") || "[]")
+    const index = notes.findIndex(note => note.title === title && note.text === text)
+
+    if (index === -1) {
         Swal.fire({
-            title: 'Ошибка',
-            text: 'Введите заголовок или текст заметки',
-            icon: 'error',
+            title: "Ошибка",
+            text: "Заметка не найдена",
+            icon: "error",
             showConfirmButton: false
         })
+    } else {
+        notes.splice(index, 1)
+        localStorage.setItem("saved-notes", JSON.stringify(notes))
+        li.remove()
 
-        return
+        Swal.fire({
+            title: "Успех",
+            text: "Заметка успешно удалена",
+            icon: "success",
+            showConfirmButton: false
+        })
     }
-    
-    // 1. получить из localStorage хранимую строку
-    // 2. распарсить (преобразовать) полученную строку в список
-    // 3. добавить в список новую заметку
-    // 4. преобразовать список обратно в строку
-    // 5. записать новую строку в localStorage
+  };
 
-    const note = { title, text}
-    const notes = JSON.parse(localStorage.getItem('saved-notes') || '[]')
-    notes.push(note)
-    localStorage.setItem('saved-notes', JSON.stringify(notes))
+  const li = document.createElement("li")
+  li.append(panelDiv, deleteIcon)
+  return li
+};
 
-    document.querySelector('main ul').append(createNoteMarkup(note))
-    closeNoteModal()
-}
+const saveNote = () => {
+  const title = notesModal.querySelector("input").value.trim();
+  const text = notesModal.querySelector("textarea").value.trim();
 
-const notes = JSON.parse(localStorage.getItem('saved-notes') || '[]')
-const ul = document.querySelector('main ul')
+  if (!title && !text) {
+    Swal.fire({
+      title: "Ошибка",
+      text: "Введите заголовок или текст заметки",
+      icon: "error",
+      showConfirmButton: false,
+    })
 
-// for..of
-for (const note of notes) {
-    ul.append(createNoteMarkup(note))
-}
+    return
+  }
 
-// ul.innerHTML = ''
+  const note = { title, text };
+  const notes = JSON.parse(localStorage.getItem("saved-notes") || "[]");
+  notes.push(note);
+  localStorage.setItem("saved-notes", JSON.stringify(notes));
 
-// forEach
-// notes.forEach(note => ul.append(createNoteMarkup(note)))
+  document.querySelector("main ul").append(createNoteMarkup(note));
+  closeNoteModal();
+};
 
-// for..of & forEach VS for
+const notes = JSON.parse(localStorage.getItem("saved-notes") || "[]");
+const ul = document.querySelector("main ul");
+notes.forEach((note) => ul.append(createNoteMarkup(note)));
 
-// const numbers = [1, 2, 3, 4, 5]
-// console.log(numbers)
 
-// for (const number of numbers) {
-//     number *= 2
-// }
+const searchNote = document.querySelector(".searchNote");
+const allNoteTitles = document.querySelectorAll(".noteTitle");
 
-// console.log(numbers.map(n => n * 2))
-// console.log(numbers)
+    searchNote.onkeyup = () => {
+      const searchStr = searchNote.value
+      allNoteTitles.forEach((item) => {
+        titleStr = item.innerText
+        if (titleStr.includes(searchStr)){
+          item.style.color = 'red'
+        }
+        else{
+          item.style.color = 'black'
+        }; 
+        });
+        
+    };
+4
 
-// const products = [
-//     {
-//         title: 'мебель',
-//         price: 75,
-//         amount: 2
-//     },
-//     {
-//         title: 'мороженое',
-//         price: 6,
-//         amount: 4
-//     }
-// ]
 
-// for (const product of products) {
-//     for (const key in product) {
-//         console.log(key)
-//         console.log(product[key])
-//     }
 
-//     console.log('\n')
-// }
 
-// const products2 = products.map(p => ({
-//     good: {
-//         name: p.title,
-//         price: p.price * 100
-//     },
-//     quantity: p.amount * 1000
-// }))
-
-// console.log(products)
-// console.log(products2)
-
-// const goods = [
-//     {
-//         good: {
-//             name: 'мебель',
-//             price: 7500
-//         },
-//         quantity: 2000
-//     },
-//     {
-//         good: {
-//             name: 'мороженое',
-//             price: 600
-//         },
-//         quantity: 4000
-//     }
-// ]
-
-// const result = JSON.parse(localStorage.getItem('name'))
-// console.log(result.name)
-// console.log(result.age)
-
-// localStorage.setItem('name', JSON.stringify({
-//     name: 'artem',
-//     age: 20
-// }))
