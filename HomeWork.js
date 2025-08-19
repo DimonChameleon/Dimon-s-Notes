c5bc05d6a119f96169222de06533167bba985405
DimonChameleon <ddryomin@gmail.com>
2025-08-19 17:31:29 +0200

    Update HomeWork.js

--- HomeWork.js
+++ HomeWork.js
@@ -1,41 +1,25 @@
-// 1)
-const inputNums = [1, 2, 3, 4, 5];
-
-// с reduce
-const sumKoefs = inputNums.reduce((acc, val) => acc + val, 0);
-console.log("Сумма с reduce:", sumKoefs);
-
-// без reduce
-let sumKoefs2 = 0;
-for (const num of inputNums) {
-  sumKoefs2 += num;
-}
-console.log("Сумма без reduce:", sumKoefs2);
-// 2)
-const humans = [
-  { age: 25, weight: 70 },
-  { age: 30, weight: 80 },
-  { age: 20, weight: 60 }
-];
-
-// с reduce
-const total = humans.reduce(
-  (acc, h) => {
-    acc.age += h.age;
-    acc.weight += h.weight;
-    return acc;
-  },
-  { age: 0, weight: 0 }
-);
-console.log("Сумма с reduce:", total);
-
-// без reduce
-let ageSum = 0;
-let weightSum = 0;
-
-for (const h of humans) {
-  ageSum += h.age;
-  weightSum += h.weight;
+function mySome(arr, value) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === value) {
+      return true;
+    }
+  }
+  return false;
 }
 
-console.log("Сумма без reduce:", { age: ageSum, weight: weightSum });
+function myEvery(arr, value) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== value) {
+      return false;
+    }
+  }
+  return true;
+}
+
+let nums = [1, 2, 3, 3, 3];
+
+console.log(mySome(nums, 2)); // true
+console.log(mySome(nums, 5)); // false
+
+console.log(myEvery(nums, 3)); // false
+console.log(myEvery([3,3,3], 3)); // true
